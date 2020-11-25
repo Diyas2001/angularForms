eb36f43e83d482b30449d81c161b44bc9ea7c392
Diyas <dias.tursunkali@gmail.com>
2020-11-25 10:57:11 +0600

    ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validator} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularForms';
+
+  contactForm = new FormGroup({
+    name: new FormControl(),
+    surname: new FormControl(),
+    email: new FormControl(),
+    password: new FormControl(),
+    });
+  name;
+  surname;
+  email;
+  password;
+
+  onSubmit() {
+    this.name = this.contactForm.get('name').value;
+    this.surname = this.contactForm.get('surname').value;
+    this.email = this.contactForm.get('email').value;
+    this.password = this.contactForm.get('password').value;
+  }
 }
